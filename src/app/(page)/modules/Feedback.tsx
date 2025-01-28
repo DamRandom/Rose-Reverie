6f4b2f5e968be3f90300ade1526879a42acbdd9d
DamRandom <britohdamian@gmail.com>
2025-01-28 16:32:53 -0500

    Responsive Feedback section

--- src/app/(page)/modules/Feedback.tsx
+++ src/app/(page)/modules/Feedback.tsx
@@ -3,21 +3,21 @@ import CommentFormSection from "./CommentFormSection";
 
 export default function Feedback() {
   return (
-    <section className="flex flex-col lg:flex-row gap-10 p-10">
-      {/* Sección de comentarios */}
-      <div
-        className="lg:w-1/2 flex items-center justify-center"
-        data-aos="fade-right"
-      >
-        <TestimonialsSection />
-      </div>
-
+    <section className="flex flex-col-reverse lg:flex-row gap-10 p-10">
       {/* Sección del formulario */}
       <div
         className="lg:w-1/2 flex items-center justify-center"
         data-aos="fade-left"
       >
         <CommentFormSection />
+      </div>
+
+      {/* Sección de comentarios */}
+      <div
+        className="lg:w-1/2 flex items-center justify-center"
+        data-aos="fade-right"
+      >
+        <TestimonialsSection />
       </div>
     </section>
   );
