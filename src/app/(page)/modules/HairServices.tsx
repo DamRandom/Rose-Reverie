04a370ecc9ba9ec73da672a847180cee35f3e8c2
DamRandom <britohdamian@gmail.com>
2025-01-28 21:35:29 -0500

    Responsive Hair Service section

--- src/app/(page)/modules/HairServices.tsx
+++ src/app/(page)/modules/HairServices.tsx
@@ -11,7 +11,6 @@ const services = [
         et de la forme de votre visage.
       </>
     ),
-    imageSrc: "/images/haircut.jpg",
   },
   {
     name: "Coiffure",
@@ -22,7 +21,6 @@ const services = [
         stylistes transformeront vos cheveux en une œuvre d&apos;art personnalisée.
       </>
     ),
-    imageSrc: "/images/styling.jpg",
   },
   {
     name: "Coloration",
@@ -33,23 +31,22 @@ const services = [
         créer des couleurs vibrantes et durables qui vous feront briller.
       </>
     ),
-    imageSrc: "/images/coloring.jpg",
   },
 ];
 
 const HairServices = () => {
   return (
     <section className="py-16">
-      <div className="container mx-auto px-6 space-y-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 space-y-8">
         {services.map((service, index) => (
           <div
             key={index}
             className="relative flex flex-col items-center space-y-6"
           >
             {/* Card with Frosted Glass Effect */}
-            <div className="relative p-8 w-full h-60 rounded-lg shadow-lg bg-white bg-opacity-0 backdrop-blur-lg backdrop-brightness-75 border border-white/30 z-10">
-              <h3 className="text-3xl font-semibold text-white">{service.name}</h3>
-              <p className="text-lg mt-4 text-white">{service.description}</p>
+            <div className="relative p-6 sm:p-8 w-full max-w-2xl h-auto sm:h-60 rounded-lg shadow-lg bg-white bg-opacity-0 backdrop-blur-lg backdrop-brightness-75 border border-white/30 z-10">
+              <h3 className="text-2xl sm:text-3xl font-semibold text-white">{service.name}</h3>
+              <p className="text-base sm:text-lg mt-4 text-white">{service.description}</p>
             </div>
           </div>
         ))}
